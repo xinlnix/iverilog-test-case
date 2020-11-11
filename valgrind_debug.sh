e42f9dbae5612ab54de8220a2d94a4270934490b
xinlnix <ggg1766612538@gmail.com>
2020-11-11 18:22:28 +0800

    valgrind

--- valgrind_debug.sh
+++ valgrind_debug.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ../iverilog_src/my_iverilog/ivlpp/ivlpp $1 > temp.v #~/project/hardware/iverilog-test-case/case1011/case2/has_multi_driver/case2/multiply_large.v > temp.v #4way_4word.v > temp.v 
-valgrind --tool=callgrind ../my_iverilog_src/iverilog/ivl temp.v
+valgrind --tool=callgrind ../iverilog_src/my_iverilog/ivl temp.v
 ls -lt |awk 'NR==2{print}' |  awk '{printf $9}'  | xargs kcachegrind {} &
 
 
